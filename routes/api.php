790ca06907f880941d2fbd1fbb48811aab8453d1
nvn01234 <nguyenvannhat152@gmail.com>
2017-02-01 14:05:28 +0700

    api middleware

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/faq/search', 'Api\FaqApiController@search')->name('api.faq.search');
-Route::post('/faq/delete', 'Api\FaqApiController@destroy')->name('api.faq.delete');
+Route::post('/faq/delete', 'Api\FaqApiController@destroy')->name('api.faq.delete')->middleware('auth');
 
 Route::get('/article', 'Api\ArticleApiController@index')->name('api.article.index');
 Route::get('/article/{id}', 'Api\ArticleApiController@show')->name('api.article.show');
